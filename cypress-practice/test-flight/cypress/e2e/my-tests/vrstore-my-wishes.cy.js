64c96b73a3207a6500d2b81a979493b90adc098d
VR-Store-Neural <vrstoreneural@gmail.com>
2024-04-23 14:27:45 +0300

    cypress-project

---
+++ cypress-practice/test-flight/cypress/e2e/my-tests/vrstore-my-wishes.cy.js
@@ -0,0 +1,53 @@
+/// <reference types="cypress" />
+
+it("sort products by price down-up", () => {
+  cy.visit("https://vr-store.com.ua/");
+  cy.viewport(1920, 1080);
+
+  // Вхід в особистий кабінет
+
+  cy.get(".userbar__button").click();
+  cy.get("#login_form_id > .form > :nth-child(2) > .field").type(
+    "brown@yahoo.com"
+  );
+  cy.get("#login_form_id > .form > :nth-child(4) > .field").type(
+    "Aquapark_3000"
+  );
+  cy.get("#login_form_id > .form > .__submit > .btn > .btn-input").click();
+  cy.wait(1000);
+
+  // Перевірка входу в особистий кабінет
+
+  cy.get('.userbar__button[title="Brown"]').should("exist");
+  cy.log("Авторизація пройшла успішно");
+
+  // Пошук товару
+
+  cy.get(".search__input").type("ecoflow");
+  cy.get(".search__button").click();
+
+  // Додавання товарів до обраного
+
+  let addedProducts = 0; // Лічильник доданих товарів
+
+  cy.get(".catalog-grid__item").each(($element) => {
+    cy.wrap($element).then(($item) => {
+      if ($item.find(":contains('В желания')").length > 0) {
+        if (addedProducts < 2) {
+          cy.wrap($item).contains("В желания").click({ force: true }); // Клікаємо на кнопку "В желания"
+          cy.get("body").click("topLeft").wait(1000);
+          addedProducts++; // Збільшуємо лічильник доданих товарів
+        } else {
+          return false; // Завершуємо цикл, якщо додано два товари
+        }
+      }
+    });
+  });
+
+  // Перевірка листа бажань
+
+  cy.get(".favorites-view__icon").click();
+  cy.contains("Список желаний").should("exist");
+  const expectedItemCount = 2; // Задана кількість елементів
+  cy.get(".catalog-grid__item").should("have.length", expectedItemCount);
+});
---
+++ cypress-practice/test-flight/cypress/e2e/my-tests/vrstore-registration.cy.js
@@ -0,0 +1,24 @@
+/// <reference types="cypress" />
+
+it("registration", () => {
+  cy.visit("https://vr-store.com.ua/");
+  cy.viewport(1920, 1080);
+  cy.get(".lang-menu__item.is-active .lang-menu__link").then(($element) => {
+    if (!$element.text().includes("Укр")) {
+      cy.contains("Укр").click({ force: true });
+    } else {
+      cy.log("Українську мову вже обрано.");
+    }
+  });
+  cy.get(".userbar__button").click();
+  cy.get('[href="#j-popup-tab-signup"] > .login-tabs-txt').click();
+  cy.get("#signup-form > .form > :nth-child(2) > .field").type("Browl");
+  cy.get("#signup-form > .form > :nth-child(4) > .field").type("browl@yahoo.com");
+  cy.get(":nth-child(6) > .field").type("Aquapark_3000");
+  cy.get("#signup-form > .form > .__submit > .btn > .btn-input").click();
+  cy.get('.popup-confirm > .btn > .btn-content').should('exist').click();
+  cy.wait(1000);
+  cy.get('.userbar__button').trigger('mouseover');
+  cy.contains('Особисті дані').click();
+  cy.get('.main-h').should('contain', 'Особисті дані')
+});
